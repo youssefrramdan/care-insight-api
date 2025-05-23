2c9257fb22ce342a5ed953a6216774d6ff2a5e02
youssefrramdan <youfahmed353@gmail.com>
2025-05-23 18:39:09 +0300

    Refactor getMe function in user controller: remove specialty population to simplify user retrieval process.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -133,10 +133,7 @@ const deleteUser = asyncHandler(async (req, res, next) => {
  * @access  Private
  */
 const getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user._id).populate({
-    path: 'specialty',
-    select: 'name',
-  });
+  const user = await User.findById(req.user._id)
 
   if (!user) {
     return next(new ApiError('User not found', 404));
