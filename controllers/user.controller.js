d51bd530658879cffa2249882a83d688b179e033
youssefrramdan <youfahmed353@gmail.com>
2025-05-26 01:02:14 +0300

    Enhance getMe function in user controller: add population for doctors to include fullName, email, and profileImage for a more comprehensive user response.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -142,10 +142,15 @@ const deleteUser = asyncHandler(async (req, res, next) => {
  * @access  Private
  */
 const getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user._id).populate({
-    path: 'specialty',
-    select: 'name description',
-  });
+  const user = await User.findById(req.user._id)
+    .populate({
+      path: 'specialty',
+      select: 'name description',
+    })
+    .populate({
+      path: 'doctors',
+      select: 'fullName email profileImage',
+    });
 
   if (!user) {
     return next(new ApiError('User not found', 404));
